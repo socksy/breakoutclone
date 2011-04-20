de6ad19a798ccdadcd691947a099e9762216427f
Ben Lovell (ben j lovell <Ben Lovell (ben.j.lovell@gmail.com)>
2011-04-20 12:32:01 +0100

    Adding files forgot to add

---
+++ Level.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class Level {
+	ArrayList<int[]> outline = new ArrayList<int[]>();
+	Block[] block_index;
+	String block_texture_path;
+	String background_texture_path;
+}
