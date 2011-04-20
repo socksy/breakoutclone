d9f78276c9ad843a3b0cea671d26985f7dd248c6
Ben Lovell <ben.j.lovell@gmail.com>
2011-04-20 05:42:44 +0100

    Basic level system in place, intro to report

--- Model.java
+++ Model.java
@@ -1,4 +1,8 @@
 import java.util.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import com.google.gson.*;
 
 public class Model {
@@ -34,7 +38,7 @@ public class Model {
 
 		ball = new Ball();
 		paddle = new Paddle();
-		populateBlockArray();
+		populateBlockArray(readFile("level1.json"));
 
 		ball_x=ball.getX();
 		ball_y=ball.getY();
@@ -44,11 +48,23 @@ public class Model {
 		controller = new Controller(ball, paddle, blocks);
 	}
 
-	public void populateBlockArray() {
+	/** Populates blocks ArrayList with predesigned level
+	 * JSON, index array of different block types, array of outline of level design using numbers to refer to the index array 
+	 * (see Level.java for data structure)
+	 * @param Level String, consisting of required JSON
+	 */
+	public void populateBlockArray(String level) {
+	
+		Gson gson = new Gson();
+		Level lev = gson.fromJson(level, Level.class);
+
 		//Standard boring grid
-		for (int i=0; i<4; i++) {
+		for (int i=0; i<lev.outline.size(); i++) {
 			for (int j=0; j<13; j++) {
-				blocks.add(new Block(10+(j*60),(i*25)+10,4-i));
+				int b_index = lev.outline.get(i)[j];
+				if (b_index ==-1) { continue; }
+				//Create block, laid out so 13 per row, rows 25 px apart, columns 5px apart
+				blocks.add(new Block(10+(j*60),(i*25)+10,lev.block_index[b_index].getCurrentStrength(), lev.block_index[b_index].getColour()));
 			}
 		}
 	}
@@ -59,8 +75,27 @@ public class Model {
 		ball_y = ball.getY();
 		
 		paddle_x = paddle.getX();
-		//TODO call controller methods to find out input DONE
-		//TODO set ball vector FOAS:MDGADFGP)
-		//TODO destroy or do whatever with blocks
+		//TODO detect when level has been complete, advance to next 
 	}
+
+	/** Reads file from String
+	 * Taken from http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from-the-contents-of-a-file
+	 * @param path to read from
+	 */
+	private static String readFile(String path) {
+		try {
+			FileInputStream stream = new FileInputStream(new File(path));
+			try {
+				FileChannel fc = stream.getChannel();
+				MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+				return Charset.defaultCharset().decode(bb).toString();
+			} finally {
+				stream.close();
+			}
+		} catch (IOException ioe) {
+			System.err.println("File not found!");
+			return null;
+		}
+	}
+	
 }
